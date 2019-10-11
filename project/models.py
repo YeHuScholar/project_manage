f5c47dae5c8f970a00832b9a9592dcd8054d5962
XYRecluse <lixy2017@aliyun.com>
2019-10-12 07:49:26 +0800

    调整代码格式

--- project/models.py
+++ project/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(default="项目名称：xxx", max_length=50)
     price = models.DecimalField(default=0, max_digits=12, decimal_places=2)
-    update_time = models.DateTimeField(auto_now=True,auto_now_add =False)
-    date_time = models.DateTimeField(auto_now=False,auto_now_add =True)
+    update_time = models.DateTimeField(auto_now=True, auto_now_add=False)
+    date_time = models.DateTimeField(auto_now=False, auto_now_add=True)
     # id = models.UUIDField(primary_key=True, editable=False)
 
     def __str__(self):
